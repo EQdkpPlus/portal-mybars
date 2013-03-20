4983e7bc6b9a2da106359e87371010734e5840fe
godmod <godmod@eqdkp-plus.eu>
2013-03-20 09:43:20 +0000

    Ported to 1.1

---
+++ language/german.php
@@ -0,0 +1,34 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2008
+ * Date:		$Date: 2013-01-04 23:03:33 +0100 (Fr, 04 Jan 2013) $
+ * -----------------------------------------------------------------------
+ * @author		$Author: hoofy_leon $
+ * @copyright	2006-2011 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev: 12732 $
+ * 
+ * $Id: german.php 12732 2013-01-04 22:03:33Z hoofy_leon $
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+
+$lang = array(
+	'mybars'					=> 'Meine Fortschritts-Balken',
+	'mybars_name'				=> 'Eigene Fortschritts-Balken',
+	'mybars_desc'				=> 'Gib einen eigenen Fortschritts-Balken auf der Seite aus',
+	'pk_mybars_title'			=> 'Balken-Text (Balken %d)',
+	'pk_mybars_current'			=> 'aktueller Fortschrittswert (Balken %d)',
+	'pk_mybars_max'				=> 'maximaler Fortschrittswert (Balken %d)',
+	'pk_mybars_tooltip'			=> 'Gib den Inhalt des Tooltips ein (Balken %d)',
+	'pk_mybars_headtext'		=> 'Titel des Moduls',
+	'pk_mybars_bars'			=> 'Anzahl an Balken'
+);
+?>
