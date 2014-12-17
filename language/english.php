6ec85bc107ccaf654fe8a90266f5a79c73e0ef83
godmod <godmod@eqdkp-plus.eu>
2014-12-17 20:34:39 +0000

    Added languages

--- language/english.php
+++ language/english.php
@@ -1,6 +1,6 @@
 <?php
 /*	Project:	EQdkp-Plus
- *	Package:	MyBars Portal Module
+ *	Package:	EQdkp-Plus Language File
  *	Link:		http://eqdkp-plus.eu
  *
  *	Copyright (C) 2006-2015 EQdkp-Plus Developer Team
@@ -19,19 +19,27 @@
  *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-if ( !defined('EQDKP_INC') ){
-	header('HTTP/1.0 404 Not Found');exit;
+ 
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
 }
 
-$lang = array(
-	'mybars'				=> 'My Progress-Bar',
-	'mybars_name'			=> 'Progress-Bar',
-	'mybars_desc'			=> 'Display your own Progress-Bar on the page',
-	'mybars_f_title'		=> 'Title of the Progress-Bar (Bar %d)',
-	'mybars_f_current'		=> 'Current progress (Bar %d)',
-	'mybars_f_max'			=> 'Maximum progress (Bar %d)',
-	'mybars_f_tooltip'		=> 'Enter a tooltip for the bar (Bar %d)',
-	'mybars_f_headtext'		=> 'Title of the module',
-	'mybars_f_bars'			=> 'Number of bars',
+//Language: English	
+//Created by EQdkp Plus Translation Tool on  2014-12-17 21:28
+//File: portal/mybars/language/english.php
+//Source-Language: german
+
+$lang = array( 
+	"mybars" => 'My Progress-Bar',
+	"mybars_name" => 'Progress-Bar',
+	"mybars_desc" => 'Display your own Progress-Bar on the page',
+	"mybars_f_title" => 'Title of the Progress-Bar (Bar %d)',
+	"mybars_f_current" => 'Current progress (Bar %d)',
+	"mybars_f_max" => 'Maximum progress (Bar %d)',
+	"mybars_f_tooltip" => 'Enter a tooltip for the bar (Bar %d)',
+	"mybars_f_headtext" => 'Title of the module',
+	"mybars_f_bars" => 'Number of bars',
+	
 );
+
 ?>
